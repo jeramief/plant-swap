b1d99d59a466f22ce4c891326651e1e0971ad51f
Jeramie <forbesjeramie@gmail.com>
2024-04-01 23:19:21 -0400

    pushing to production - trying removing references to get production working

--- backend/db/migrations/20240301192321-create-membership.js
+++ backend/db/migrations/20240301192321-create-membership.js
@@ -12,10 +12,10 @@ module.exports = {
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: {
-          model: "Users",
-          key: "id",
-        },
+        // references: {
+        //   model: "Users",
+        //   key: "id",
+        // },
       },
       groupId: {
         allowNull: false,
